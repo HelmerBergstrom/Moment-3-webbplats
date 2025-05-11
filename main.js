b1a0b25e5b9f78191b6a3d48866e15f4dbb931a3
Helmer Bergström <hebe2400@student.miun.se>
2025-05-11 20:23:38 +0200

    Lägger till funktion för att radera erfarenhet.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ if(list) {
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            if(data.length === 0) {
+            if(!data) {
                 list.innerHTML = `<p> Inga erfarenhter finns att hämta! 
                 Lägg till under fliken <a href="add.html"> LÄGG TILL </a></p>`
             }
@@ -18,9 +18,36 @@ if(list) {
                 <h2> ${erf.task} </h2>
                 <p><strong> ${erf.city} </strong></p>
                 <p><strong> ${erf.howlong} </strong></p>
+                <button class="delete-btn" data-id="${erf._id}"> RADERA </button> 
                 `
 
                 list.appendChild(article);
+
+                const deleteButton = article.querySelector(".delete-btn");
+
+                deleteButton.addEventListener("click", async () => {
+                    const id = deleteButton.getAttribute("data-id");
+
+                    const confirmDelete = confirm("Är du säker?");
+                    if(!confirmDelete) return;
+
+                    try {
+                        const response = await fetch(`http://127.0.0.1:3001/workexperience/${id}`, {
+                        method: "DELETE"    
+                    });
+
+                    const result = await response.json();
+                    if(response.ok) {
+                        article.remove();
+                        alert(result.message);
+                    } else {
+                        alert(result.message);
+                    }
+
+                    } catch(error) {
+                        console.log("Fel vid borttagning av erfarenhet: " + error);
+                    }
+                })
             });
         })
 }
