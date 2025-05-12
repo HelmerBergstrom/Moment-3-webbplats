42b54ab70a846bd03b1f941bb817dfec2bd8b1b1
Helmer Bergström <hebe2400@student.miun.se>
2025-05-12 13:17:34 +0200

    Skapar formulär och funktion för att lägga till en erfarenhet i listan på startsidan.

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const list = document.getElementById("experienceList");
 
 if(list) {
-    let url = "http://127.0.0.1:3001/workexperience";
+    let url = "http://127.0.0.1:3002/workexperience";
     
     fetch(url)
         .then(response => response.json())
         .then(data => {
+            console.log("Data från servern:", data);
             if(!data) {
                 list.innerHTML = `<p> Inga erfarenhter finns att hämta! 
                 Lägg till under fliken <a href="add.html"> LÄGG TILL </a></p>`
@@ -16,18 +17,19 @@ if(list) {
                 if(erf.howlongM === 0) {
                     article.innerHTML = `
                         <h2> ${erf.companyname} </h2>
-                        <h2> ${erf.task} </h2>
-                        <p><strong> ${erf.city} </strong></p>
-                        <p><strong> ${erf.howlongY} år </strong></p>
-                        <button class="delete-btn" data-id="${erf._id}"> RADERA </button> 
+                        <h3> ${erf.task} </h3>
+                        <p><strong> Vart: ${erf.city} </strong></p>
+                        <p><strong> Tid: ${erf.howlongY} år </strong></p>
+                        <button class="delete-btn" data-id="${erf._id}"> RADERA 🗑️ </button> 
                     `
                 } else {
                     article.innerHTML = `
                         <h2> ${erf.companyname} </h2>
-                        <h2> ${erf.task} </h2>
-                        <p><strong> ${erf.city} </strong></p>
-                        <p><strong> ${erf.howlongY} år och ${erf.howlongM} månader </strong></p>
-                        <button class="delete-btn" data-id="${erf._id}"> RADERA </button> 
+                        <h3> ${erf.task} </h3>
+                        <p><strong> Vart: ${erf.city} </strong></p>
+                        <p><strong> Tid: ${erf.howlongY} år och ${erf.howlongM} månader </strong></p>
+                        <button class="delete-btn" data-id="${erf._id}"> RADERA 🗑️ </button> 
+
                     `
                 }
 
@@ -36,20 +38,21 @@ if(list) {
                 const deleteButton = article.querySelector(".delete-btn");
 
                 deleteButton.addEventListener("click", async () => {
-                    const id = deleteButton.getAttribute("data-id");
+                    
 
                     const confirmDelete = confirm("Är du säker?");
                     if(!confirmDelete) return;
 
+                    const id = deleteButton.getAttribute("data-id");
+
                     try {
-                        const response = await fetch(`http://127.0.0.1:3001/workexperience/${id}`, {
+                        const response = await fetch(`http://127.0.0.1:3002/workexperience/${id}`, {
                         method: "DELETE"    
                     });
 
                     const result = await response.json();
                     if(response.ok) {
                         article.remove();
-                        alert(result.message);
                     } else {
                         alert(result.message);
                     }
@@ -60,4 +63,49 @@ if(list) {
                 })
             });
         })
+}
+
+const form = document.getElementById("expForm");
+const submitBtn = document.getElementById("submit");
+const confirmMessage = document.getElementById("confirm");
+
+if(form) {
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+    
+    const newExp = {
+        companyname: form.companyname.value,
+        task: form.task.value,
+        city: form.city.value,
+        howlongY: form.howlongY.value,
+        howlongM: form.howlongM.value
+    };
+
+    if (!newExp.companyname || !newExp.task || !newExp.city || !newExp.howlongY || !newExp.howlongM) {
+        alert("Fyll i alla obligatoriska fält!");
+        return;
+    };
+
+    try {
+        const response = await fetch("http://127.0.0.1:3002/workexperience", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json" 
+            },
+            body: JSON.stringify(newExp)
+        });
+
+        const result = await response.json();
+        if(response.ok) {
+            confirmMessage.textContent = result.message;
+            window.location.href = "index.html";
+        } else {
+            alert("Fel! Försök igen senare.")
+            console.log(result)
+        }
+    } catch(error) {
+        console.error("Fel vid postning av erfarenhet:", error);
+        alert("Något gick fel vid inmatning, försök igen.");
+    }
+    })
 }
